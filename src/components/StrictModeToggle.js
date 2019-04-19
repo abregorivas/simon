c498876e548e54b9b403da935246a16a29af7f98
Roland Abregorivas <abregorivas@gmail.com>
2019-04-19 12:53:36 -0700

    add strictmodetoggle component

---
+++ src/components/StrictModeToggle.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const StrictModeToggle = ({ handleToggle, strict }) => {
+  return (
+    <div className="onoffswitch">
+      <input
+        type="checkbox"
+        name="onoffswitch"
+        className="onoffswitch-checkbox"
+        id="myonoffswitch"
+        checked={strict}
+        readOnly
+        onChange={handleToggle}
+      />
+      <label className="onoffswitch-label" htmlFor="myonoffswitch">
+        <span className="onoffswitch-inner" />
+        <span className="onoffswitch-switch" />
+      </label>
+    </div>
+  );
+};
+
+StrictModeToggle.propTypes = {
+  strict: PropTypes.string,
+  handleToggle: PropTypes.func
+};
+
+export default StrictModeToggle;
